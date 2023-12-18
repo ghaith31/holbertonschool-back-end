1d5b78afb216770628db082246b2561f47820948
ghayth31 <gharsallighaith31@gmail.com>
2023-12-18 16:06:28 +0100

    the 1 api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-
 """
-Retrieve employee TODO list progress using REST API
+Retrieves TODO list progress for a given employee ID from a REST API.
 """
 
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    response = requests.get(base_url)
+def fetch_employee_todo_list(employee_id):
+    base_url = 'https://jsonplaceholder.typicode.com'
+    todos_url = f'{base_url}/todos?userId={employee_id}'
+    user_url = f'{base_url}/users/{employee_id}'
 
-    if response.status_code != 200:
-        print("Error:", response.json().get('message', 'Failed to retrieve data.'))
+    try:
+        todos_response = requests.get(todos_url)
+        user_response = requests.get(user_url)
+        todos_response.raise_for_status()
+        user_response.raise_for_status()
+    except requests.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
         return
 
-    todos = response.json()
-    completed_tasks = [task for task in todos if task['completed']]
-    total_tasks = len(todos)
-    num_completed_tasks = len(completed_tasks)
-    employee_name = todos[0]['username'] if todos else 'Unknown'
+    todos_data = todos_response.json()
+    user_data = user_response.json()
 
-    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
-    for task in completed_tasks:
+    employee_name = user_data.get('name')
+    total_tasks = len(todos_data)
+    done_tasks = [task for task in todos_data if task['completed']]
+    num_done_tasks = len(done_tasks)
+
+    print(f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
         print(f"\t{task['title']}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
     else:
         employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+        fetch_employee_todo_list(employee_id)
